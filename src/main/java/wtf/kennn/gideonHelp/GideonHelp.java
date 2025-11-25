90a40e8093a2840b703f0e2a0c31a409e86a6292
KenziiiEngineer <kenziiicontacto@gmail.com>
2025-11-24 19:14:06 -0600

    Refactor GideonHelp for better organization and logging
    Refactor GideonHelp class to improve structure and logging.

--- src/main/java/wtf/kennn/gideonHelp/GideonHelp.java
+++ src/main/java/wtf/kennn/gideonHelp/GideonHelp.java
@@ -2,20 +2,16 @@ package wtf.kennn.gideonHelp;
 
 import co.aikar.commands.PaperCommandManager;
 import dev.dejvokep.boostedyaml.YamlDocument;
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import wtf.kennn.gideonHelp.Commands.AdminCommand;
 import wtf.kennn.gideonHelp.Commands.HelpCommand;
 import wtf.kennn.gideonHelp.Listener.CommandBlocker;
 import wtf.kennn.gideonHelp.Listener.TabCompleteBlocker;
-import wtf.kennn.gideonHelp.Util.YamlUtil;
+import wtf.kennn.gideonHelp.Util.ChatUtil;
 
-
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-
-import static wtf.kennn.gideonHelp.Util.ChatUtil.log;
 
 public class GideonHelp extends JavaPlugin {
 
@@ -26,59 +22,109 @@ public class GideonHelp extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        saveDefaultConfig();
+
+        // 🌟 Tu mensaje bonito
+        ChatUtil.log("&b--------------------------------------------");
+        ChatUtil.log("&a         GideonHelp is starting...");
+        ChatUtil.log("&7        Thank you for using my plugin!");
+        ChatUtil.log("&b--------------------------------------------");
+
+        // ⚙️ CARGA CONFIG
+        saveDefaultConfig(); // solo si NO existe
+
+        // ⚙️ CARGA MENU.YML
         loadMenuFile();
 
-        commandManager = new PaperCommandManager(this);
-        commandManager.getCommandCompletions().registerStaticCompletion(
-                "helpModes",
-                Arrays.asList("TEXT", "MENU", "COMMAND")
-        );
-        commandManager.registerCommand(new HelpCommand());
-        commandManager.registerCommand(new AdminCommand());
+        // 📦 REGISTRO DE COMANDOS Y EVENTOS
+        registerCommands();
+        registerListeners();
 
-        Bukkit.getPluginManager().registerEvents(new CommandBlocker(), this);
-        Bukkit.getPluginManager().registerEvents( new TabCompleteBlocker(), this);
-
-        log("       &b&lGIDEONHELP");
-        log("");
-        log("&fPlugin by &bGideon Studio");
-        log("&fAuthor: &bKenn");
-        log("&fStatus: &aEnabled");
-        log("&fVersion: &a" + getDescription().getVersion());
-        log("");
-
+        ChatUtil.log("&aGideonHelp has been enabled successfully!");
     }
 
     @Override
     public void onDisable() {
-        log("       &b&lGIDEONHELP");
-        log("");
-        log("&fPlugin by  &bGideon Studio");
-        log("&fAuthor: &bKenn");
-        log("&fStatus: &cDisabled");
-        log("&fVersion: &a" + getDescription().getVersion());
-        log("");
+        ChatUtil.log("&cGideonHelp has been disabled.");
     }
 
-    public void loadMenuFile() {
-        menuFile = new YamlUtil(this).load("menu.yml");
-    }
+    // -----------------------------------------------------
+    //             CARGA INICIAL DEL MENU.YML
+    // -----------------------------------------------------
+    private void loadMenuFile() {
+        File file = new File(getDataFolder(), "menu.yml");
 
-    public void reloadAll() {
-        reloadConfig();
+        if (!file.exists()) {
+            ChatUtil.log("&eCreating default menu.yml...");
+            saveResource("menu.yml", false);
+        }
+
         try {
-            menuFile.reload();
+            menuFile = YamlDocument.create(file, getResource("menu.yml"));
         } catch (IOException e) {
             e.printStackTrace();
+            getLogger().severe("Could not load menu.yml!");
         }
     }
 
+    // -----------------------------------------------------
+    //                   RELOAD SIN BORRAR
+    // -----------------------------------------------------
+    public void reloadAll() {
+        reloadConfig(); // NO reescribe config
+
+        try {
+            if (menuFile != null) {
+                menuFile.reload(); // NO reescribe menu.yml
+            } else {
+                loadMenuFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            getLogger().severe("Could not reload menu.yml!");
+        }
+
+        // 🌟 Tu mensaje bonito después de recargar
+        ChatUtil.log("&b--------------------------------------------");
+        ChatUtil.log("&a   Configuration files reloaded successfully!");
+        ChatUtil.log("&b--------------------------------------------");
+    }
+
+    // -----------------------------------------------------
+    //                  COMANDOS
+    // -----------------------------------------------------
+    private void registerCommands() {
+        commandManager = new PaperCommandManager(this);
+
+        // Autocompletado TAB para /ghelp mode
+        commandManager.getCommandCompletions().registerStaticCompletion(
+                "helpModes",
+                Arrays.asList("TEXT", "MENU", "COMMAND")
+        );
+
+        commandManager.registerCommand(new HelpCommand());
+        commandManager.registerCommand(new AdminCommand());
+    }
+
+    // -----------------------------------------------------
+    //                  LISTENERS
+    // -----------------------------------------------------
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new CommandBlocker(), this);
+        getServer().getPluginManager().registerEvents(new TabCompleteBlocker(), this);
+    }
+
+    // -----------------------------------------------------
+    //                   GETTERS
+    // -----------------------------------------------------
     public static GideonHelp getInstance() {
         return instance;
     }
 
     public YamlDocument getMenuFile() {
         return menuFile;
+    }
+
+    public PaperCommandManager getCommandManager() {
+        return commandManager;
     }
 }
